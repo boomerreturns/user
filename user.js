c64c9203101fd07a954e8ea46c1b3d6a6a25b5f7
Rich Gautereaux <richgautereaux@gmail.com>
2017-09-15 09:42:56 -0700

    initial userpage commit

---
+++ user.js
@@ -0,0 +1,71 @@
+$(() => {
+  console.log('document is ready!')
+  $('.userChoice').on('click', event => {
+    event.preventDefault()
+    let userData = {};
+    $('input').each((index, field) => {
+      userData[field.id] = $(field).val();
+    })
+
+    console.log(dataIsInvalid(userData), userData);
+    let formattedData = !dataIsInvalid(userData) ? formatData(userData) : null;
+
+    if (formattedData) {
+      let finalForm = {
+        user: formattedData
+      }
+      finalForm = JSON.stringify(finalForm);
+      swal({
+        title: 'Awesome!',
+        text: 'You are now registered and can start making orders!',
+        type: 'success',
+        confirmButtonText: "Let's Go!",
+        onClose: redirectToOrders
+      })
+      // $.post('https://boomerreturns.herokuapp.com/v0/users', finalForm).done(response => {
+      //   console.log(response);
+      //
+      // });
+    }
+  })
+
+  function redirectToOrders(){
+    location.href = 'order.html'
+  }
+
+
+  function formatData(data){
+    let formattedData = {
+      name: '',
+      phone: '',
+      address: ''
+    };
+    Object.keys(data).forEach(key => {
+      switch (key) {
+        case 'firstName':
+        case 'middleName':
+          formattedData.name += data[key] + ' ';
+          break;
+        case 'lastName':
+          formattedData.name += data[key];
+          break;
+        case 'streetAddress':
+        case 'cityName':
+        case 'stateCode':
+          formattedData.address += data[key] + ', ';
+          break;
+        case 'zipcode':
+          formattedData.address += data[key];
+          break;
+        case 'phoneNumber':
+          formattedData.phone = data[key];
+          break;
+        default:
+          break;
+      }
+    })
+    return formattedData
+  }
+
+  let dataIsInvalid = data => Object.keys(data).some(key => !data[key]);
+})
